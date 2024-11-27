de588798c10dea3db07d858aa508050ad9ce1f24
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-11-27 13:51:30 +1100

    task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -18,14 +18,6 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
@@ -33,11 +25,19 @@ export default class HolbertonCourse {
     this._name = newName;
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = Number(newLength);
+  }
+
+  get students() {
+    return this._students;
   }
 
   set students(newStudents) {
