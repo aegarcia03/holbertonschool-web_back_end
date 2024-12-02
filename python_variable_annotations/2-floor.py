f2fa7b17eaa51f605b86b76d244ff349fb8ba42e
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-02 16:48:55 +1100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""This module uses type annotations."""
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    result = math.trunc(n)
+    return result
