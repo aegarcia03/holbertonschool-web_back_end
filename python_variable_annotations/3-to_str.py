c66fe60a309ee9916d389b49a13ec453ab6831bf
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-02 16:53:11 +1100

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module uses type annotations."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float"""
+    result = str(n)
+    return result
