ff8eefd7955a6cd460105028ccb073b36baf78d4
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-02 16:32:26 +1100

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """This module provides basic arithmetic operations using type annotations."""
+
+
 def add(a: float, b: float) -> float:
     """Returns the sum of two arguments as a float"""
     return a + b
