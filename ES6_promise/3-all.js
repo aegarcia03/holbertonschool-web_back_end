b17fb71da66fe5a24176d388e33d2d5143b3ec68
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-05 17:00:05 +1100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,4 +7,5 @@ export default function handleProfileSignup() {
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName}, ${results[1].lastName}`);
     }).catch(() => console.log('Signup system offline'));
+  return promises;
 }
