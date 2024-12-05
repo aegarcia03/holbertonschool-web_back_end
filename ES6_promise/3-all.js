5f47d314effcc55b032e83586d999c5ddd7cf88b
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-06 10:02:23 +1100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,21 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
+  const user = createUser();
+  const photo = uploadPhoto();
 
-  Promise.all(promises)
-    .then((results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }).catch(() => console.log('Signup system offline'));
-  return promises;
+  let output = '';
+
+  return photo
+  .then((photoData) => {
+    output += photoData.body;
+    return user;
+  })
+  .then((user) => {
+    output += `${user.firstName} ${user.lastName}`;
+    console.log(output);
+  })
+  .catch(() => {
+  console.log('Signup system offline');
+  });
 }
