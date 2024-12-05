9d7184e1d0a9eb1ae05570832bb5726b9fadbe27
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-05 15:50:57 +1100

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,9 +2,9 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
-      body: 'Success',
+      body: 'success',
     }))
-    .catch(() => new Error())
+    .catch(() => Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
