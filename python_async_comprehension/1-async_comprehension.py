95ae0095f59902605e1ce8b7d999ea248f82e09b
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-11 21:44:49 +1100

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
-    return [ for x in async_generator]
+    return [_ async for _ in async_generator()]
