8ea4fb5ef226fe79d8bb3481d6852a5b1158b8b8
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-11 13:37:17 +1100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 Async Comprehension."""
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+
+async def async_generator() -> Generator[float, None, None]:
     """Defines a coroutine that loops 10 times"""
     for i in range(10):
-        await asyncio.sleep(1)
-
         yield (random.uniform(0, 10))
+        await asyncio.sleep(1)
