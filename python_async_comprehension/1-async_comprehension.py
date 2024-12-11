9df5359ed577e7cc9c8644434ab8926cf9831e0a
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-11 21:50:25 +1100

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """This module demonstrates the basics of
 Async Comprehension."""
-import random
 import asyncio
-from typing import AsyncGenerator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
+    """Uses async comprehension to collect 10 random numbers."""
     return [_ async for _ in async_generator()]
