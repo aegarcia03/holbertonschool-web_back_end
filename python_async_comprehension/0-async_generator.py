9370deb7ca01183b3a737b7ad5df616502ccc5e9
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-11 16:54:31 +1100

    task 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import AsyncGenerator, Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Defines a coroutine that loops 10 times"""
-    for i in range(10):
+    for _ in range(10):
         yield (random.uniform(0, 10))
         await asyncio.sleep(1)
---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module demonstrates the basics of
+Async Comprehension."""
+import random
+import asyncio
+from typing import AsyncGenerator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    return [ for x in async_generator]
