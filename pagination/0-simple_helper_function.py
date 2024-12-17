789a3ebc2548017b36554fa9695fcada85d3a154
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-18 01:42:00 +1100

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,9 +3,9 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns the range of indexes for a particular
     pagination parameters"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return [start_index, end_index]
+    return (start_index, end_index)
