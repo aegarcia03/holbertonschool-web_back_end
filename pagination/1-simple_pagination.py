b6e0f4f7a421d9ac3334ebe5c4ef300c9e1128fd
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-12-18 01:48:20 +1100

    task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""This module works pagination"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the range of indexes for a particular
+    pagination parameters"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
