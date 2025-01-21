b5c608ef778086dc461adb3cd49a16944fe1f14a
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-01-21 11:02:31 +1100

    task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  let responseMessage = 'This is the list of our students\n';
+  try {
+    const output = await countStudents(process.argv[2]);
+    responseMessage += output.join('\n');
+  } catch (error) {
+    responseMessage = 'Cannot load the database';
+  }
+  res.send(responseMessage); // Send a single response
+});
+
+app.listen(PORT);
+
+module.exports = app;
