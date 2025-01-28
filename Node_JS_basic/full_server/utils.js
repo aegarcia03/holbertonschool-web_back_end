c75d80ffc643b1f7893c3742b68a391e04dc9924
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-01-28 12:12:23 +1100

    task 8

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,31 +1,23 @@
 const fs = require('fs').promises;
 
-async function readDatabase(path) {
-  try {
-    // Read the file content asynchronously
-    const data = await fs.readFile(path, { encoding: 'utf-8' });
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const rows = lines.slice(1); // Exclude the header
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim());
 
-    const studentsCS = []; // Initialize array for CS students
-    const studentsSWE = []; // Initialize array for SWE students
+      const fields = {};
+      students.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
 
-    for (const row of rows) {
-      const columns = row.split(',');
-      const studentName = columns[0];
-      const field = columns[3];
-    
-      if (field === 'CS') studentsCS.push(studentName);
-      if (field === 'SWE') studentsSWE.push(studentName);
-    }
-
-    return {
-      CS: studentsCS,
-      SWE: studentsSWE,
-    };
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-};
+      resolve(fields);
+    })
+    .catch(() => reject(new Error('Cannot load the database')));
+});
 
 module.exports = readDatabase;
